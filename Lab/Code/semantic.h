01646ec91cff509c339fdc935fb1a38cdc86755f
lzk <lvpp98@gmail.com>
2019-04-17 16:23:59 +0800

    Start Lab2

---
+++ Lab/Code/semantic.h
@@ -0,0 +1,6 @@
+#ifndef SEMANTIC_H
+#define SEMANTIC_H
+
+#include "table.h"
+
+#endif
---
+++ Lab/Code/table.h
@@ -0,0 +1,13 @@
+/*
+    Data structure of symbol table
+    Use PJW's hash function and open hashing method
+*/  
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "tree.h"
+#include "semantic.h"
+
+
+
+#endif
