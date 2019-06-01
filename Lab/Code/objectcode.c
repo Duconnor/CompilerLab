bdf367ec094008faa1c1f15f70f734c65605bd1d
lzk <lvpp98@gmail.com>
2019-06-01 16:52:28 +0800

    add some data structures

---
+++ Lab/Code/objectcode.c
@@ -0,0 +1,1 @@
+#include "objectcode.h"
---
+++ Lab/Code/objectcode.h
@@ -0,0 +1,41 @@
+#ifndef OBJECTCODE_H
+#define OBJECTCODE_H
+
+#include "intercode.h"
+#include <stdio.h>
+
+typedef struct mVar_* mVar;
+
+struct mVar_ {
+    char* name;
+    int offset;
+    mVar next;
+};
+
+void putMVar(mVar v);
+mVar getMVar(char* name);
+
+void mAlloc(char* varName, int size);
+
+char* getVarName(Operand op);
+
+void printObjectCode(InterCode ic, FILE* fp);
+void mPrintLABEL(InterCode ic, FILE* fp);
+void mPrintFUNCTION(InterCode ic, FILE* fp);
+void mPrintASSIGN(InterCode ic, FILE* fp);
+void mPrintASSIGNP(InterCode ic, FILE* fp);
+void mPrintPASSIGN(InterCode ic, FILE* fp);
+void mPrintADD(InterCode ic, FILE* fp);
+void mPrintSUB(InterCode ic, FILE* fp);
+void mPrintMUL(InterCode ic, FILE* fp);
+void mPrintDIV(InterCode ic, FILE* fp);
+void mPrintGOTO(InterCode ic, FILE* fp);
+void mPrintIFGOTO(InterCode ic, FILE* fp);
+void mPrintRETURN(InterCode ic, FILE* fp);
+void mPrintDEC(InterCode ic, FILE* fp);
+void mPrintARGV(InterCode ic, FILE* fp);
+void mPrintCALL(InterCode ic, FILE* fp);
+void mPrintREAD(InterCode ic, FILE* fp);
+void mPrintWrite(InterCode ic, FILE* fp);
+
+#endif
