6a6fa36aec5870b273bb0def26168bb22ed9e6f1
lzk <lvpp98@gmail.com>
2019-06-09 19:59:42 +0800

    add FUNCTION, PARAM, RETURN

--- Lab/Code/objectcode.c
+++ Lab/Code/objectcode.c
@@ -10,6 +10,8 @@ static mVar mVarList = NULL;
 
 static int offset = 0;
 
+static int pCount = 0;
+
 static void loadVar(char *varName, int varSize, int regNum, FILE *fp) {
 	/* This function load a var from memory into a register */
 	/* 'regNum' is the number of the register you want to store */
@@ -26,7 +28,7 @@ static void loadVar(char *varName, int varSize, int regNum, FILE *fp) {
 	} else {
 		offset = v->offset;
 	}
-	sprintf(code, "\tlw $%d, -%d($fp)\n", regNum, offset);
+	sprintf(code, "\tlw $%d, %d($fp)\n", regNum, offset);
 	fputs(code, fp);
 }
 
@@ -44,7 +46,7 @@ static void saveVar(char *varName, int varSize, int regNum, FILE *fp) {
 	} else {
 		offset = v->offset;
 	}
-	sprintf(code, "\tsw $%d, -%d($fp)\n", regNum, offset);
+	sprintf(code, "\tsw $%d, %d($fp)\n", regNum, offset);
 	fputs(code, fp);
 }
 
@@ -223,7 +225,7 @@ void mPrintSUB(InterCode ic, FILE* fp) {
     char* op2Name = getVarName(op2);
     if(op2->kind == CONSTANT) {
         loadVar(op1Name, 4, 8, fp);
-        sprintf(line, "\taddi $9, $8, -%d\n", op2->u.value);
+        sprintf(line, "\taddi $9, $8, %d\n", -op2->u.value);
         fputs(line, fp);
         saveVar(resName, 4, 9, fp);
     } else {
@@ -338,11 +340,44 @@ void mPrintIFGOTO(InterCode ic, FILE *fp) {
 }
 
 void mPrintFUNCTION(InterCode ic, FILE* fp) {
-
+    char line[100];
+    memset(line, 0, sizeof(line));
+    sprintf(line, "%s:\n", ic->u.sinop.op->u.name);
+    fputs(line, fp);
+    sprintf(line, "\taddi $sp, $sp, -4\n");
+    fputs(line, fp);
+    sprintf(line, "\tsw $fp, 0($sp)\n");
+    fputs(line, fp);
+    sprintf(line, "\tmove $fp, $sp\n");
+    fputs(line, fp);
 }
 
-void mPrintRETURN(InterCode ic, FILE* fp) {
+void mPrintPARAM(InterCode ic, FILE* fp) {
+    mVar v = (mVar)malloc(sizeof(struct mVar_));
+    v->name = getVarName(ic->u.sinop.op);
+    v->offset = 8 + 4 * pCount;
+    v->next = NULL;
+    putMVar(v);
+    if(ic->next->kind != PARAM)
+        pCount = 0;    
+}
 
+
+void mPrintRETURN(InterCode ic, FILE* fp) {
+    char* retVarName = getVarName(ic->u.sinop.op);
+    mVar v = getMVar(retVarName);
+    char line[100];
+    memset(line, 0, sizeof(line));
+    sprintf(line, "\tlw $v0, %d($fp)\n", v->offset);
+    fputs(line, fp);
+    sprintf(line, "\tmove $sp, $fp\n");
+    fputs(line, fp);
+    sprinft(line, "\tlw $fp, 0($sp)\n");
+    fputs(line, fp);
+    sprintf(line, "\taddi $sp, $sp, 4\n");
+    fputs(line, fp);
+    sprintf(line, "\tjr $ra\n");
+    fputs(line, fp);
 }
 
 void mPrintDEC(InterCode ic, FILE* fp) {
@@ -353,12 +388,13 @@ void mPrintARGV(InterCode ic, FILE* fp) {
 
 }
 
+
 void mPrintCALL(InterCode ic, FILE* fp) {
 
 }
 
 void mPrintREAD(InterCode ic, FILE* fp) {
-    
+
 }
 
 void mPrintWRITE(InterCode ic, FILE* fp) {
